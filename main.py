5ff5c61eabe14c3e5f58c3270a851d65ef1f8d74
OvdiienkoAndrew <ovdienkoandrej57@gmail.com>
2026-03-12 12:21:22 +0200

    minus_errors

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
+from tkinter import filedialog
+
+import numpy as np
+from PIL import Image
 
 if __name__ == '__main__':
-    from src.models.main.main import Main
-    from src.menu.convert.main import main_menu
-    main = Main()
-    main_menu(main)
-    main.run()
+    # from src.models.main.main import Main
+    # from src.menu.convert.main import main_menu
+    # main = Main()
+    # main_menu(main)
+    # main.run()
+    file_path = filedialog.askopenfilename(
+        title="Оберіть зображення",
+        filetypes=[
+            ("Усі зображення", "*.bmp *.png *.gif *.tiff *jpg")
+        ]
+    )
+    img = Image.open(file_path)
+
+    array = np.array(img)
+
+    middle = array.mean(axis=2)
+
+    array[:, :, 0] = middle
+    array[:, :, 1] = middle
+    array[:, :, 2] = middle
+
+    img = Image.fromarray(array)
+
+    img.save(f"{file_path[:file_path.rfind('.')]}_demo{file_path[file_path.rfind('.'):]}")
+
+
+
+
+# синий негатив
+# array[:, :, 0] = 255-middle
+# array[:, :, 1] = 255-middle
+# array[:, :, 2] = 255
+
+# обычное но с накладкой сверху желтого и синего
+# array[:, :, 0] = middle
+# array[:, :, 1] = middle
+# array[:, :, 2] = 128
+
+# сине жолтий
+# array[:, :, 0] = middle
+# array[:, :, 1] = middle
+# array[:, :, 2] = 255-middle
+
+# желто синий
+# array[:, :, 0] = 255-middle
+# array[:, :, 1] = 255-middle
+# array[:, :, 2] = middle
--- src/menu/tasks/first_lab/functions/task/do/main.py
+++ src/menu/tasks/first_lab/functions/task/do/main.py
@@ -1,8 +1,57 @@
+import multiprocessing
+import numpy as np
+from PIL import Image
+from tkinter import messagebox
+
+def proces_1(file_path,array,width):
+
+    mask = array.mean(axis=2) < 128
+    array[mask,:] = (0, 255, 0)
+    array[~mask,:] = (255, 255, 255)
+
+    for i in range(4,width,20):
+        array[:,i:i+5] = (255,255,0)
+
+    img = Image.fromarray(array)
+
+    img.save(f"{file_path[:file_path.rfind('.')]}_such_binary{file_path[file_path.rfind('.'):]}")
+
+
+def proces_2(file_path,array,width):
+
+    middle = ((array[:, :,0].astype(int) + array[:, :,2].astype(int))//2).astype(int)
+
+    array[:, :,0] = middle
+    array[:, :,1] = 255
+    array[:, :,2] = middle
+
+
+    for i in range(4, width, 20):
+        array[:, i:i + 5] = (255, 255, 0)
+
+    img = Image.fromarray(array)
+
+
+    img.save(f"{file_path[:file_path.rfind('.')]}_such_2_middle{file_path[file_path.rfind('.'):]}")
+
+
+def proces_3(file_path,array,width):
+
+    middle = array.mean(axis=2)
+
+    array[:, :, 0] = middle
+    array[:, :, 1] = 255
+    array[:, :, 2] = middle
+
+    for i in range(4, width, 20):
+        array[:, i:i + 5] = (255, 255, 0)
+
+    img = Image.fromarray(array)
+
+    img.save(f"{file_path[:file_path.rfind('.')]}_such_3_middle{file_path[file_path.rfind('.'):]}")
 
 
 def do(main, file_path):
-    from PIL import Image
-    from tkinter import messagebox
 
     if not file_path:
         messagebox.showerror("Помилка", "Файл не обрано!")
@@ -10,18 +59,22 @@ def do(main, file_path):
         return
 
     img = Image.open(file_path)
-    pixels = img.load()
 
-    width, height = img.size
+    array = np.array(img)
 
-
-    for i in range(4,width,20):
-        for k in range(i, min(i + 6, width)):
-            for j in range(height):
-                pixels[k, j] = (185,239, 201, 255)
+    p1 = multiprocessing.Process(target=proces_1, args=(file_path,array,array.shape[1]))
+    p2 = multiprocessing.Process(target=proces_2, args=(file_path,array,array.shape[1]))
+    p3 = multiprocessing.Process(target=proces_3, args=(file_path,array,array.shape[1]))
 
 
-    img.save(f"{file_path[:file_path.rfind('.')]}_convert{file_path[file_path.rfind('.'):]}")
+    p1.start()
+    p2.start()
+    p3.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+
 
     main.end()
     return
